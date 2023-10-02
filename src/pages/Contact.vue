75a5a04e700cf52ce8cb9bc6cd0ca171e165136e
Nima Karimi <nimakarimi676@gmail.com>
2023-10-02 16:38:28 +0200

    added Home and contact

---
+++ src/pages/Contact.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const user = useUserStore()
+
+interface FormData {
+  name: string
+  email: string
+  message: string
+}
+const formData = ref<FormData>({
+  name: user.savedName,
+  email: '',
+  message: '',
+})
+// let formError : Ref<string> =ref('');
+
+function submitForm() { }
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <TheInput v-model="formData.name" :placeholder="t('form.name')" autocomplete="true" @keydown.enter="submitForm" />
+
+    <TheInput
+      v-model="formData.email" :placeholder="t('form.email')" autocomplete="true" type="email"
+      @keydown.enter="submitForm"
+    />
+
+    <textarea
+      v-model="formData.message" :placeholder="t('form.message')" text="center" h-26 bg="transparent"
+      border="~ rounded blue-200 dark:blue-500" outline="none active:none"
+    />
+
+    <button text-m m-3 w-40 btn @click="submitForm">
+      <!-- :disabled="!name || !email || message.length < 1 " -->
+      {{ t('button.send') }}
+    </button>
+  </form>
+</template>
+
+
+<style scoped>
+form {
+    max-width: 500px;
+    margin: 0 auto;
+    border: 1.5px solid #ddd;
+    border-radius: 15px;
+    padding: 20px;
+}
+
+input,
+textarea {
+    width: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+    margin-bottom: 20px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,14 +5,7 @@ defineOptions({
 </script>
 
 <template>
-  <div id="home">
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-
-    <div py-4 />
-
-    <Contact id="contact" />
-    <WhoWeAre id="who" />
-  </div>
+  <Home id="home" />
+  <Contact id="contact" />
+  <WhoWeAre id="who" />
 </template>
