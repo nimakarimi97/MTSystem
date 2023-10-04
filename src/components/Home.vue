d021cf843061e6844cccc28c72197fe541f7dcb6
Nima Karimi <nimakarimi676@gmail.com>
2023-10-04 12:30:21 +0200

    added home title and image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,26 @@
-<template>
-  <div
-        
-    m-1 mb-9 border-pink-9 rounded-2 p-4 opacity-75 transition-all-900 hover:mx-9 hover:border-1 hover:rounded-10 hover:bg-gray-8 hover:p-1 hover:opacity-100 
-  >
-    <div text-6xl>
-      <div i-carbon-campsite inline-block />
-    </div>
+<script setup>
+const { t } = useI18n()
+const imgSrc = ref(isDark.value ? 'MTDark.png' : 'MTLight.png')
 
-    <div class="hero">
-      <h1 text-3xl>
-        Welcome to the Hero Section
-      </h1>
-      <div class="welcome-message" mt-12 p-5>
-        <img src="logo-light.jpeg" alt="Welcome Image">
-        <p>This is a welcome message.</p>
-      </div>
+watchEffect(() => {
+  imgSrc.value = isDark.value ? 'MTDark.png' : 'MTLight.png'
+})
+</script>
+
+<template>
+  <div          
+    mb-9 flex flex-col items-center justify-center gap-10 border-pink-9 rounded-2 p-4 px-12 opacity-75 transition-all-900 hover:mx-9 md:flex-row hover:border-1 hover:rounded-10 hover:bg-gray-8 hover:p-1 hover:opacity-100
+  >
+    <div>
+      <h3 mb-10 class="title">
+        {{ t('home.title') }}
+      </h3>
+      <button btn>
+        {{ t('home.button') }}
+      </button>
     </div>
-    <div py-4 />
+    {{ isDark.value }}
+    <img :src="imgSrc" alt="Welcome Image" w-130>
   </div>
 </template>
 
-<style scoped>
-.welcome-message {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-}
-
-.welcome-message img {
-    width: 300px;
-    margin-right: 10px;
-}
-</style>
