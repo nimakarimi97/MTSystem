547672192a914716f5e2763f342103ffd0a5b35a
Nima Karimi <nimakarimi676@gmail.com>
2023-10-06 18:35:07 +0200

    updated who we are

--- src/pages/WhoWeAre.vue
+++ src/pages/WhoWeAre.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
-import Profit from './Profit.vue'
-
-const name: string = 'Who'
-const isContactVisible = ref(true)
-
-function alertFunc(data: string) {
-  alert(`Alert function -> ${data}`)
-}
-
-
-function showContact() {
-  isContactVisible.value = !isContactVisible.value
-}
 </script>
 
 <template>
-  <div my-5 rounded-7 bg-green-950 p-8>
-    <button mb-11 bg-orange-5 btn @click="showContact">
-      {{ isContactVisible ? 'Hide contacts' : 'Show contacts' }}
-    </button>
-    <Profit v-if="isContactVisible" id="profit" :name="name" @customEvent="alertFunc" />
+  <div flex-center flex-col px-38 py-12 lg:flex-row-reverse>
+    <div class="w-1/2">
+      <h4>&lt;- Video</h4>
+    </div>
+    <div class="w-1/2">
+      <!-- <iframe border-2 width="560" height="315" src="" allowfullscreen></iframe> -->
+      <div h-sm w-xl border-2 src="" />
+    </div>
   </div>
 </template> 
