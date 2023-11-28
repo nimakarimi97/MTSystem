e3c8084cbb1867331fb60600f89138885b1a8134
Nima Karimi <nimakarimi676@gmail.com>
2023-11-28 22:09:40 +0100

    fix: prices bullet points

--- src/pages/Prices.vue
+++ src/pages/Prices.vue
@@ -5,29 +5,28 @@ const { t } = useI18n()
 <template>
   <div flex flex-col gap-6>
     <h2 class="title">
-      {{ t('prices.title') }}
+      {{ t("prices.title") }}
     </h2>
-    <p>{{ t('prices.body1') }}</p>
-    <h4>{{ t('prices.title2') }}</h4>
-    <p>{{ t('prices.description') }}</p>
+    <p>{{ t("prices.body1") }}</p>
+    <h4>{{ t("prices.title2") }}</h4>
+    <p>{{ t("prices.description") }}</p>
 
     <ul>
-      <li v-for="i in 8" :key="i">
-        {{ t(`prices.features[${i}]`) }}
+      <li v-for="i in 9" :key="i" py-1>
+        {{ t(`prices.features[${i - 1}]`) }}
       </li>
 
       <hr class="mx-auto w-70%" my-4>
 
       <div flex-center flex-col gap-4>
-        <span>{{ t('prices.footer') }}</span>
+        <span>{{ t("prices.footer") }}</span>
         <button btn @click="scrollToComponent('#prices')">
-          {{ t('prices.button') }}
+          {{ t("prices.button") }}
         </button>
       </div>
     </ul>
   </div>
 </template>
-
 
 <style scoped>
 /* Your component styles go here */
