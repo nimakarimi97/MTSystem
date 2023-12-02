a0914b132f3c62c28cb472821d3ac4ee87a9a0d1
Nima Karimi <nimakarimi676@gmail.com>
2023-12-02 18:26:38 +0100

    fix vps scrolling to component

--- src/pages/VPS.vue
+++ src/pages/VPS.vue
@@ -4,49 +4,51 @@ const { t } = useI18n()
 
 <template>
   <div>
-    <h2 primary-color>
-      {{ t("vps.title") }}
-    </h2>
+    <div>
+      <h2 primary-color>
+        {{ t("vps.title") }}
+      </h2>
 
-    <p>{{ t("vps.body1") }}</p>
-    <p>{{ t("vps.body2") }}</p>
-    <p>{{ t("vps.body3") }}</p>
+      <p>{{ t("vps.body1") }}</p>
+      <p>{{ t("vps.body2") }}</p>
+      <p>{{ t("vps.body3") }}</p>
 
-    <ul my-9>
-      <p>{{ t("vps.body4") }} :</p>
-      <li v-for="i in 3" :key="i" py-1 class="bullet-point">
-        {{ t(`vps.body5[${i - 1}]`) }}
-      </li>
-      <hr class="mx-auto w-70%" my-4>
-    </ul>
-  </div>
-
-  <div class="flex justify-center" gap-5>
-    <div class="card flex-center-col gap-6" border-2 border-amber-6>
-      <h4>MENSILE</h4>
-      <p>20€ al Mese + iva</p>
-      <p>(abbonamento automatico ogni mese)</p>
-      <button class="btn">
-        Acquista Ora
-      </button>
+      <ul my-9>
+        <p>{{ t("vps.body4") }} :</p>
+        <li v-for="i in 3" :key="i" py-1 class="bullet-point">
+          {{ t(`vps.body5[${i - 1}]`) }}
+        </li>
+        <hr class="mx-auto w-70%" my-4>
+      </ul>
     </div>
 
-    <div class="card flex-center-col gap-6" border-2 border-amber-6>
-      <h4>TRIMESTRALE</h4>
-      <p>50€ + iva</p>
-      <p>(abbonamento automatico ogni tre mesi)</p>
-      <button class="btn">
-        Acquista ora
-      </button>
-    </div>
+    <div class="flex justify-center" gap-5>
+      <div class="card flex-center-col gap-6" border-2 border-amber-6>
+        <h4>MENSILE</h4>
+        <p>20€ al Mese + iva</p>
+        <p>(abbonamento automatico ogni mese)</p>
+        <button class="btn">
+          Acquista Ora
+        </button>
+      </div>
 
-    <div class="card flex-center-col gap-6" border-2 border-amber-6>
-      <h4>ANNUALE</h4>
-      <p>180€ + iva</p>
-      <p>(abbonamento automatico ogni 12 mesi)</p>
-      <button class="btn">
-        Acquista ora
-      </button>
+      <div class="card flex-center-col gap-6" border-2 border-amber-6>
+        <h4>TRIMESTRALE</h4>
+        <p>50€ + iva</p>
+        <p>(abbonamento automatico ogni tre mesi)</p>
+        <button class="btn">
+          Acquista ora
+        </button>
+      </div>
+
+      <div class="card flex-center-col gap-6" border-2 border-amber-6>
+        <h4>ANNUALE</h4>
+        <p>180€ + iva</p>
+        <p>(abbonamento automatico ogni 12 mesi)</p>
+        <button class="btn">
+          Acquista ora
+        </button>
+      </div>
     </div>
   </div>
 </template>
