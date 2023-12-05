04a3a9196dcbaeea6327e2ee24ff049e2223a694
Nima Karimi <nimakarimi676@gmail.com>
2023-12-05 22:39:06 +0100

    fix an image in profit section

--- src/pages/Profit.vue
+++ src/pages/Profit.vue
@@ -12,27 +12,27 @@ const { t } = useI18n()
       <p my-5>
         {{ t("profit.body1") }}
       </p>
-      <img src="/risultati/MYFXBOOK/a.jpg" alt="">
+      <img src="/risultati/MYFXBOOK/a.jpg" alt="profit">
 
       <p my-5>
         {{ t("profit.body2") }}
       </p>
-      <img src="/risultati/MYFXBOOK/b.jpg" alt="hi">
+      <img src="/risultati/MYFXBOOK/b.jpg" alt="profit">
     </div>
 
     <div class="mx-auto my-15 w-55% items-end gap-3 px-8 lg:w-40% md:flex-center">
-      <img src="/risultati/MYFXBOOK/left.jpg" alt="" h-auto pb-4 md-h-125 md:p-0>
-      <img src="/risultati/MYFXBOOK/right.jpg" alt="" h-auto md-h-125>
+      <img src="/risultati/MYFXBOOK/left.jpg" alt="profit" h-auto pb-4 md-h-125 md:p-0>
+      <img src="/risultati/MYFXBOOK/right.jpg" alt="profit" h-auto md-h-125>
     </div>
 
     <div class="mx-auto flex flex-col gap-3 lg:w-90%">
       <h3 primary-color>
         {{ t("profit.title2") }}
       </h3>
-      <img class="max-w-2xl object-cover" src="/risultati/MT4/1.jpg" alt="">
-      <img class="max-w-2xl self-end" src="/risultati/MT4/2.jpg" alt="">
-      <img class="max-w-2xl" src="/risultati/MT4/3.jpg" alt="">
-      <img class="max-w-2xl self-end" src="/risultati/MT4/4.jpg" alt="">
+      <img class="max-w-2xl object-cover" src="/risultati/MT4/1.jpg" alt="profit">
+      <img class="max-w-2xl self-end" src="/risultati/MT4/2.jpg" alt="profit">
+      <img class="max-w-2xl" src="/risultati/MT4/3.jpg" alt="profit">
+      <img class="max-w-2xl self-end" src="/risultati/MT4/4.jpg" alt="profit">
     </div>
   </div>
 </template>
