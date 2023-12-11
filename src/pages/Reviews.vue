f0c60b3f30f1eaee4a392b0315759f3c86d3d86c
Nima Karimi <nimakarimi676@gmail.com>
2023-12-11 22:34:08 +0100

    style: header of review section

--- src/pages/Reviews.vue
+++ src/pages/Reviews.vue
@@ -4,11 +4,9 @@ const { t } = useI18n()
 
 <template>
   <div my-10 class="reviews">
-    <h5 my-10 tracking-widest class="reviews">
+    <h4 my-10 font-size-1.2rem tracking-widest primary-color class="reviews">
       {{ t("reviews.title") }}
-    </h5>
+    </h4>
     <Slideshow />
   </div>
 </template>
-
-<style scoped></style>
