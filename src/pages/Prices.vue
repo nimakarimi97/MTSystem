8b02c0763655fedbdf51c66d78b3ac0a5e228ea2
Nima Karimi <nimakarimi676@gmail.com>
2023-12-12 21:18:31 +0100

    style: new price section

--- src/pages/Prices.vue
+++ src/pages/Prices.vue
@@ -4,27 +4,44 @@ const { t } = useI18n()
 
 <template>
   <div flex flex-col gap-6>
-    <h2 class="title">
+    <h2 class="title" primary-color>
       {{ t("prices.title") }}
     </h2>
-    <p>{{ t("prices.body1") }}</p>
+
     <h4>{{ t("prices.title2") }}</h4>
     <p>{{ t("prices.description") }}</p>
 
-    <ul>
-      <li v-for="i in 9" :key="i" py-1>
+    <ul mx-auto text-left>
+      <li v-for="i in 9" :key="i" flex-center justify-start py-1>
+        <svg
+          primary-color
+          xmlns="http://www.w3.org/2000/svg"
+          width="42"
+          height="42"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="m10.5 16.2l-4-4l1.4-1.4l2.6 2.6l5.6-5.6l1.4 1.4l-7 7Z"
+          />
+        </svg>
         {{ t(`prices.features[${i - 1}]`) }}
       </li>
-
-      <hr class="mx-auto w-70%" my-4>
-
-      <div flex-center flex-col gap-4>
-        <span>{{ t("prices.footer") }}</span>
-        <button btn @click="scrollToComponent('#prices')">
-          {{ t("prices.button") }}
-        </button>
-      </div>
     </ul>
+
+    <hr class="mx-auto w-70%" my-4>
+
+    <div flex-center flex-col gap-4>
+      <span>
+        <span line-through>
+          {{ t("prices.footer2") }}
+        </span>
+        {{ t("prices.footer") }}
+      </span>
+      <button btn @click="scrollToComponent('#prices')">
+        {{ t("prices.button") }}
+      </button>
+    </div>
   </div>
 </template>
 
