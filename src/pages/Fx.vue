18377a16870eecc28ae7db3f05cf3dea544d6102
Nima Karimi <nimakarimi676@gmail.com>
2023-12-13 23:04:00 +0100

    fixed images extensions naming

--- src/pages/Fx.vue
+++ src/pages/Fx.vue
@@ -18,14 +18,14 @@ const { t } = useI18n()
         <p>
           {{ t("fx.body1").toUpperCase() }}
         </p>
-        <img src="/myfxbook/storico1sinistra.PNG" alt="myfx-aziendale">
+        <img src="/myfxbook/storico1sinistra.png" alt="myfx-aziendale">
       </div>
 
       <div flex-center-col gap-4>
         <p>
           {{ t("fx.body2").toUpperCase() }}
         </p>
-        <img src="/myfxbook/MT41destra.PNG" alt="myfx-chart">
+        <img src="/myfxbook/MT41destra.png" alt="myfx-chart">
       </div>
     </div>
 
@@ -33,16 +33,16 @@ const { t } = useI18n()
       {{ t("fx.description2") }}
     </p>
     <div class="mx-auto w-45% flex-center-col gap-12 px-8 lg:w-40% md:flex-row">
-      <img src="/myfxbook/storico2sinistra.PNG" alt="profit" h-auto>
-      <img src="/myfxbook/MT42destra.PNG" alt="profit" h-auto pb-4 md:p-0>
+      <img src="/myfxbook/storico2sinistra.png" alt="profit" h-auto>
+      <img src="/myfxbook/MT42destra.png" alt="profit" h-auto pb-4 md:p-0>
     </div>
 
     <p my-0>
       {{ t("fx.description3") }}
     </p>
     <div class="mx-auto w-45% flex-center-col gap-12 px-8 lg:w-40% md:flex-row">
-      <img src="/myfxbook/storico3sinistra.JPG" alt="profit" h-auto>
-      <img src="/myfxbook/MT43destra.PNG" alt="profit" h-auto pb-4 md:p-0>
+      <img src="/myfxbook/storico3sinistra.jpg" alt="profit" h-auto>
+      <img src="/myfxbook/MT43destra.png" alt="profit" h-auto pb-4 md:p-0>
     </div>
   </div>
 </template>
