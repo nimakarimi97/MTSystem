5c931d7145c3be5d827f9ef87e95908280924fc7
Nima Karimi <nimakarimi676@gmail.com>
2023-12-13 22:37:14 +0100

    style: added features to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,11 +13,34 @@ watchEffect(() => {
       <h3 mb-10 class="title">
         {{ t("home.title") }}
       </h3>
-      <button btn @click="scrollToComponent('#prices')">
-        {{ t("home.button") }}
-      </button>
+
+      <div>
+        <p>{{ t("prices.description") }} :</p>
+
+        <ul mx-auto text-left>
+          <li v-for="i in 9" :key="i" flex-center justify-start py-1>
+            <svg
+              primary-color
+              xmlns="http://www.w3.org/2000/svg"
+              width="42"
+              height="42"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m10.5 16.2l-4-4l1.4-1.4l2.6 2.6l5.6-5.6l1.4 1.4l-7 7Z"
+              />
+            </svg>
+            {{ t(`prices.features[${i - 1}]`) }}
+          </li>
+        </ul>
+
+        <button mt7 btn @click="scrollToComponent('#prices')">
+          {{ t("prices.button") }}
+        </button>
+      </div>
     </div>
-    {{ isDark.value }}
+
     <img :src="imgSrc" alt="Welcome Image" w-130>
   </div>
 </template>
